1504e51dc2c5a6a22229610730cb4d472844906e
suryapratap2312 <87615712+suryapratap2312@users.noreply.github.com>
2023-05-01 22:08:24 +0530

    Create Exception3.cpp

---
+++ Exception3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Welcome in main function\n";
+    try
+    {
+        cout<<"Try Block\n";
+        throw 1;
+        cout<<"After Throw\n";
+    }
+    catch(double e)
+    {
+        cout<<"Exception Handled: "<<e<<endl;
+    }
+    cout<<"End of main function\n";
+}
